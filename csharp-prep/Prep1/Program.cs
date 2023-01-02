d868a1a06c1b7bae23fae13eed6e8ad34bd93ee9
nelsonpetro <nelsonpetrodoria@gmail.com>
2023-01-02 16:57:46 -0500

    Preparation 1 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Declare the two variables to use
+        String firstName, lastName;
+        //Ask the user to enter his/her first name on the same line
+        Console.Write("What is your first name? ");
+        //Get the input from the user
+        firstName = Console.ReadLine();
+        //Ask the user to enter his/her last name on the same line
+        Console.Write("What is your last name? ");
+        //Get the input from the user
+        lastName = Console.ReadLine();
+        //Print the final sentence
+        Console.WriteLine($"Your name is {char.ToUpper(lastName[0])}{lastName.Substring(1)}, {char.ToUpper(firstName[0])}{firstName.Substring(1)} {char.ToUpper(lastName[0])}{lastName.Substring(1)}.");
+         
     }
 }
