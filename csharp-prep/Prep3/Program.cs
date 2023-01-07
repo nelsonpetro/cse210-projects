63f858cd0238c1d08b4d26b23fbbdd646efaa6c3
nelsonpetro <nelsonpetrodoria@gmail.com>
2023-01-07 13:33:07 -0500

    magic number program addded

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?: ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        int guess;
+        do
+        {
+            Console.Write("What is your guess: ");
+            guess = int.Parse(Console.ReadLine());
+            Console.Clear();
+            if(magicNumber == guess)
+            {
+                Console.Write("You guessed it!");
+            }
+            else
+            {
+                Console.WriteLine($"Your guess was {guess}");
+                if(magicNumber > guess)
+                {
+                    Console.WriteLine("Try again with a higher number");
+                }
+                else
+                {
+                    Console.WriteLine("Try again with a lower number");
+                }
+            }
+        }while(magicNumber != guess);
     }
 }
