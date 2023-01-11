9970d56cbf3d8c91b03a3f0daefcfceccf63b783
nelsonpetro <nelsonpetrodoria@gmail.com>
2023-01-10 20:55:00 -0500

    comments added

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,21 +1,32 @@
 using System;
 
 class Program
-{
+{   
     static void Main(string[] args)
     {   
+        //create a variable named answer to handle
+        //the continuity of the code if the user wants to
         string answer = "yes";
+        //do while loop to handle the first part
+        //of the code
         do
         {
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
-        int guess;
-        int attempts = 1;
+            //Generates a random number between 1 and 100
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            //declare variablesto store the guess and attempts the user
+            //took to guess the correcct number
+            int guess;
+            int attempts = 1;
+            //do while to handle the guessing part of the code
             do
             {
+                //prompt the user to write a guess
                 Console.Write("What is your guess: ");
                 guess = int.Parse(Console.ReadLine());
+                //clear the terminal
                 Console.Clear();
+                //compare the guess to the random number
                 if(magicNumber == guess)
                 {
                     Console.WriteLine("You guessed it!");
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -11,30 +11,33 @@ class Program
         int squaredNumber = SquareNumber(favoriteNumber);
         DisplayResult(name, squaredNumber);
     }
+    //function to print the welcome message
     static void DisplayWelcome()
     {
         Console.WriteLine("Welcome to the program!");
     }
-
+    //function to prompt the user to enter his/her name
     static string PromptUserName()
     {
         Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
         return name;
     }
-
+    //function to prompt the user to enter his/her favorite
+    //number
     static int PromptUserNumber()
     {
         Console.Write("Please enter your favorite number: ");
         int number = int.Parse(Console.ReadLine());
         return number;
     }
-
+    //function to get the number and reuturn it squared
     static int SquareNumber(int number)
     {
         return number*number;
     }
-
+    //function to print the result as desired
+    //"user's name" the square of your number is "squared number".
     static void DisplayResult(string name, int squaredNumber)
     {
         Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
