9fc675c1d1059b92eadfb1ed42a6fbe773cdfb6e
nelsonpetro <nelsonpetrodoria@gmail.com>
2023-01-30 21:37:29 -0500

    learning 3 activity

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,48 @@
+using System;
+
+public class Fraction
+{
+    int _top;
+    int _bottom;
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+    //Getters and setters
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    public string GetFractionString()
+    {
+        string fraction = $"{_top}/{_bottom}";
+        return fraction;
+    }
+    public double GetDecimalValue()
+    {
+        return Math.Round(((double)_top / (double)_bottom),3);
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -3,7 +3,25 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
+    {   
+        int answer = 1;
+        do
+        {
+            Console.Clear();
+            Fraction frac = new Fraction();
+            Console.Write("Please, enter the numerator: ");
+            int numerator = int.Parse(Console.ReadLine());
+            Console.Write("Please, enter the denominator: ");
+            int denominator = int.Parse(Console.ReadLine());
+            frac.SetTop(numerator);
+            frac.SetBottom(denominator);
+            frac.GetTop();
+            frac.GetBottom();
+            Console.WriteLine("");
+            Console.WriteLine(frac.GetFractionString());
+            Console.WriteLine(frac.GetDecimalValue());
+            Console.Write("\nPlease enter 1 to use again, any other number to quit: ");
+            answer = int.Parse(Console.ReadLine());
+        }while(answer == 1);
     }
 }
