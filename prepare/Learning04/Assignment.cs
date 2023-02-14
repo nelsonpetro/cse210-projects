98d3138fdb053881b6182cbf6f4c7a55d513a32b
nelsonpetro <nelsonpetrodoria@gmail.com>
2023-02-13 23:06:50 -0500

    learning activity 4

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,45 @@
+using System;
+
+public class Assignment
+{
+    private string _studentName;
+
+    private string _topic;
+
+    public Assignment()
+    {
+        _studentName = "";
+        _topic = "";
+    }
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public void SetStudentName(string studentName)
+    {
+        _studentName = studentName;
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public void SetTopic(string topic)
+    {
+        _topic = topic;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
+    }    
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.Write("Enter 1 for math assignment and 2 for writting assignment: ");
+
+        int choice = int.Parse(Console.ReadLine());
+
+        Console.Write("\nPlease enter the student's name: ");
+
+        string name = Console.ReadLine();
+
+        Console.Write("\nPlease enter the topic: ");
+
+        string topic = Console.ReadLine();
+
+        switch (choice)
+        {
+            case 1:
+                Console.Write("\nPlease enter the textbook section: ");
+
+                string textBookSection = Console.ReadLine();
+
+                Console.Write("\n Please enter the problems: ");
+
+                string problems = Console.ReadLine();
+
+                MathAssignment mathAssignment = new MathAssignment(name, topic, textBookSection, problems);
+
+                Console.WriteLine(mathAssignment.GetSummary());
+
+                Console.Write(mathAssignment.GetHomeworkList());
+
+                break;
+
+            case 2:
+
+                Console.Write("\nPlease enter the title: ");
+
+                string title = Console.ReadLine();
+
+                WritingAssignment writingAssignment = new WritingAssignment(name, topic, title);
+
+                Console.WriteLine(writingAssignment.GetSummary());
+
+                Console.Write(writingAssignment.GetWritingInfo());
+
+                break;             
+        }     
     }
 }
